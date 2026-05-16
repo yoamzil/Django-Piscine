93ce07dec8bfb72290682f3296bf09c251bcf720
Youness Amzil <amzilyouness@gmail.com>
2026-05-16 11:20:38 +0100

    Using dict comprehension

--- Django-0-Starting/ex04/state.py
+++ Django-0-Starting/ex04/state.py
@@ -2,31 +2,24 @@ import sys
 
 def find_state():
     states = {
-        "Oregon" : "OR",
-        "Alabama" : "AL",
-        "New Jersey": "NJ",
-        "Colorado" : "CO"
+        "Oregon": "OR", "Alabama": "AL",
+        "New Jersey": "NJ", "Colorado": "CO"
     }
     capital_cities = {
-        "OR": "Salem",
-		"AL": "Montgomery",
-		"NJ": "Trenton",
-		"CO": "Denver"
-	}
+        "OR": "Salem", "AL": "Montgomery",
+        "NJ": "Trenton", "CO": "Denver"
+    }
 
     if len(sys.argv) == 2:
-        for each in capital_cities:
-            if sys.argv[1] in capital_cities[each]:
-                abrv = each
-                for each in states:
-                    if abrv in states[each]:
-                        print(each)
-                        break
-                else:
-                    print("Unknown capital city")
-                break
+        inverted = {v: k for k, v in capital_cities.items()}
+        if sys.argv[1] in inverted:
+            abrv = inverted[sys.argv[1]]
+            for state, code in states.items():
+                if code == abrv:
+                    print(state)
+                    break
         else:
-            print("unknown capital city")
+            print("Unknown capital city")
 
 if __name__ == '__main__':
 	find_state()
